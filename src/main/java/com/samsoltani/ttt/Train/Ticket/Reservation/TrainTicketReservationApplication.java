a957749b15ff91fe4e7d86141d79e188ee876303
Ramin Zare <zare88@fastmail.com>
2022-05-27 19:22:04 +0200

    First commit, project structure

---
+++ src/main/java/com/samsoltani/ttt/Train/Ticket/Reservation/TrainTicketReservationApplication.java
@@ -0,0 +1,13 @@
+package com.samsoltani.ttt.Train.Ticket.Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsoltani/ttt/Train/Ticket/Reservation/TrainTicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsoltani.ttt.Train.Ticket.Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
